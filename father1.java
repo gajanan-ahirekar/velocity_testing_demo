bc701ca520dd076976433987e48f022c4beab2f8
Gajanan <gajananaherekar23@gmail.com>
2022-07-26 13:02:49 +0530

    commiting java files

---
+++ father1.java
@@ -0,0 +1,15 @@
+package Home_Work;
+
+public class father1 {
+	public void money() {
+		System.out.println("Father1 have 10k");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Running in Father class");
+	
+
+	}
+
+}
---
+++ son1.java
@@ -0,0 +1,15 @@
+package Home_Work;
+
+public class son1 extends father1 {
+	public void money() {
+		System.out.println("Son1 have 3k");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		son1 obj = new son1();
+		obj.money();
+
+	}
+
+}
